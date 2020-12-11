b0f277de818344d8fd27873a76c25919b81b0863
white-jang <galaxy__12@naver.com>
2020-12-11 23:45:41 +0900

    css/js 분리

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function next() {
+    document.getElementById("new_item").src = "./images/newitem.png";
+}
+function prev() {
+    document.getElementById("new_item").src = "./images/newitem2.png";
+}
